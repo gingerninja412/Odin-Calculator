28b09467335af75a2895aa0312224cbd74b8dcb7
gingerninja412 <samdpeevor@gmail.com>
2023-09-15 09:49:42 +0100

    added the screen display

--- script.js
+++ script.js
@@ -7,23 +7,42 @@ const sumDisplay = $("#sumDisplay");
 const numberDisplay = $("#numberDisplay");
 
 //control variables
-
+const letters = /[A-Za-z]/;
+const symbols = /[+\-/*]/;
+const numbersCheck = /[0-9]/;
 //processing functions
 
 //button Functions
 numbers.click(function () {
   let text = numberDisplay.text();
+  if (letters.test(text) == true) {
+    numberDisplay.empty();
+    text = "";
+    console.log(letters.test(text));
+  }
   text = text + $(this).text();
   numberDisplay.text(text);
 });
 
 operations.click(function () {
-  let symbols = /[+\-/*]/;
   let string = sumDisplay.text();
-  if (numberDisplay.text().length == 0) {
+  let number = numberDisplay.text();
+  let operation = $(this).text();
+  if (numberDisplay.text().length == 0 || letters.test(number) == true) {
     numberDisplay.text("Error no number entered");
-  }
-  if (symbols.test(string[string.length - 1]) == true) {
-    numberDisplay.text("input a number not operation");
+  } else {
+    if ($(this).attr("id") == "multiply") {
+      string = string + ` ${number} *`;
+      sumDisplay.text(string);
+      numberDisplay.empty();
+    } else if ($(this).attr("id") == "divide") {
+      string = string + ` ${number} /`;
+      sumDisplay.text(string);
+      numberDisplay.empty();
+    } else {
+      string = string + ` ${number} ${operation}`;
+      sumDisplay.text(string);
+      numberDisplay.empty();
+    }
   }
 });
